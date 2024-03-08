10adfdbd09ece5e1b51ce0422b6ee937839a34ea
DukeDaxen <162347121+DukeDaxen@users.noreply.github.com>
2024-03-08 19:56:49 +0100

    Add files via upload

---
+++ wallpaper changer.ps1
@@ -0,0 +1,24 @@
+# Setze den Bildlink
+$BildLink = "URL_DES_BILDES"
+
+# Setze den Speicherort für das temporäre Bild
+$TempBild = "$env:TEMP\hintergrundbild.jpg"
+
+# Lade das Bild von der URL herunter
+Invoke-WebRequest -Uri $BildLink -OutFile $TempBild
+
+# Setze das Hintergrundbild
+SystemParametersInfo(20, 0, $TempBild, 0x01)
+
+# Funktion zum Setzen des Hintergrundbildes
+function SystemParametersInfo($uiAction, $uiParam, $pvParam, $fWinIni) {
+    Add-Type @"
+        using System;
+        using System.Runtime.InteropServices;
+        public class Wallpaper {
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+        }
+"@
+    [Wallpaper]::SystemParametersInfo($uiAction, $uiParam, $pvParam, $fWinIni)
+}
